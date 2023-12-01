4654c1d8146d1d567bcc0ad4779d500cdc217ea5
Zainab Jimoh <zainab.jimoh@adyen.com>
2023-12-01 17:20:27 +0100

    Revert "Release 0.5.0"
    This reverts commit a97af9557999cbc863aa9ce13e5e549c524de2b2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,12 +14,12 @@ val localProperties = Properties().apply {
 
 android {
     namespace = "com.adyen.sampleapp"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.adyen.sampleapp"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -67,9 +67,11 @@ androidComponents {
 
 dependencies {
 
-    val adyenPosMobileVersion = "0.5.0"
+    val adyenPosMobileVersion = "0.4.2"
     debugImplementation("com.adyen.ipp:pos-mobile-debug:$adyenPosMobileVersion")
+    debugImplementation("com.adyen.ipp:card-reader-management-debug:$adyenPosMobileVersion")
     releaseImplementation("com.adyen.ipp:pos-mobile-release:$adyenPosMobileVersion")
+    releaseImplementation("com.adyen.ipp:card-reader-management-release:$adyenPosMobileVersion")
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
