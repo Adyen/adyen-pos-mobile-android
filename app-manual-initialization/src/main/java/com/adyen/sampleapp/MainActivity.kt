880f52752ab0e7fe16adc8f238a927bd83b6d960
pavelbayburtyan <116361059+pavelbayburtyan@users.noreply.github.com>
2025-02-14 15:34:48 +0100

    Release/2.0.0 (#30)
    * Changes for SDK 2.0.0
    
    * Removed a file from .idea from version control

--- app-manual-initialization/src/main/java/com/adyen/sampleapp/MainActivity.kt
+++ app-manual-initialization/src/main/java/com/adyen/sampleapp/MainActivity.kt
@@ -5,10 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
-import androidx.startup.AppInitializer
-import com.adyen.ipp.InPersonPayments
-import com.adyen.ipp.InPersonPaymentsInitializer
-import com.adyen.ipp.cardreader.bluetooth.ui.DeviceManagementActivity
+import com.adyen.ipp.cardreader.api.ui.DeviceManagementActivity
 import com.adyen.sampleapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
