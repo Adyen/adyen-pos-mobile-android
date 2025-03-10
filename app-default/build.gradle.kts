54c16cc87ee2dffcbbd3889d4691c68f0ea21b10
Caio Faustino <caio.faustino@adyen.com>
2025-03-10 13:34:18 +0100

    Update targetSDK, AGP, and some libs.
    Signed-off-by: Caio Faustino <caio.faustino@adyen.com>

--- app-default/build.gradle.kts
+++ app-default/build.gradle.kts
@@ -17,12 +17,12 @@ val localProperties = Properties()
 
 android {
     namespace = "com.adyen.sampleapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.adyen.sampleapp"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app-dynamic/build.gradle.kts
+++ app-dynamic/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.adyen.sampleapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.adyen.sampleapp"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
